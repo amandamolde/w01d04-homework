d94f44a9dccd556c242f2a115826b502bea5fe26
Amanda Molde <amanda.molde@gmail.com>
2018-06-14 16:19:54 -0600

    Commit 1 - Verbal questions

---
+++ js/solutions.js
@@ -0,0 +1,3 @@
+// 1. Setup
+// 2. Parameters are names listed in a function definition while arguments are the real values passed to the function.
+// 3. The return statement stops the execution of a function and returns a value to the function caller. console.log is used to display data from Javascript.
