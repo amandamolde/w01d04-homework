9f049b34486ec251009e49e8494da728864c66c0
Amanda Molde <amanda.molde@gmail.com>
2018-06-14 16:42:37 -0600

    Commit 2 - Palindrome

--- js/solutions.js
+++ js/solutions.js
@@ -1,3 +1,19 @@
 // 1. Setup
 // 2. Parameters are names listed in a function definition while arguments are the real values passed to the function.
 // 3. The return statement stops the execution of a function and returns a value to the function caller. console.log is used to display data from Javascript.
+
+// Palindrome
+
+const checkPalindrome = (string) => {
+	let lowerCase = string.toLowerCase();
+	let reverseString = lowerCase.split('').reverse('').join('');
+	if (lowerCase === reverseString) {
+		return true
+	} else {
+		return false
+	}
+} 
+
+console.log(checkPalindrome("Radar"));
+
+console.log(checkPalindrome("Borscht"));
